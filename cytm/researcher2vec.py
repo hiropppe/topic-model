4a4a6d20ffd3ebcb65f0fd5974e76b62c59bc3a5
hiropppe <sergei.0910@gmail.com>
2021-06-27 22:43:55 +0000

    batch encode

--- cytm/researcher2vec.py
+++ cytm/researcher2vec.py
@@ -13,15 +13,18 @@ class Researcher2Vec():
 
     def __init__(self,
                  corpus,
-                 user2doc,
+                 user2doc=None,
                  K = 100,
                  shift=1,
+                 lowercase=True,
+                 max_df=1.0,
+                 min_df=1,
                  max_features=None):
         self.K = K
         self.shift = shift
         self.max_features = max_features
         
-        self.cv = CountVectorizer(binary=True, lowercase=True, max_features=self.max_features)
+        self.cv = CountVectorizer(binary=True, lowercase=lowercase, max_df=max_df, min_df=min_df, max_features=self.max_features)
         Co = self.cv.fit_transform((tokens.strip() for tokens in tqdm(corpus))).astype(np.float32)
         Y = sppmis(Co, self.shift, eps)
         U, S, V = svds(Y, k=self.K)
@@ -29,20 +32,27 @@ class Researcher2Vec():
         self.document_vectors = np.dot(U, np.sqrt(np.diag(S)))
         self.R = np.linalg.solve(np.dot(W.T, W), W.T)
 
-        self.users = list(user2doc.keys())
-        self.user2id, user_vectors = {}, []
-        for i, (user, indices) in enumerate(user2doc.items()):
-            user_vectors.append(self.document_vectors[indices].mean(axis=0))
-            self.user2id[user] = i
-        self.user_vectors = np.array(user_vectors)
+        if user2doc:
+            self.users = list(user2doc.keys())
+            self.user2id, user_vectors = {}, []
+            for i, (user, indices) in enumerate(user2doc.items()):
+                user_vectors.append(self.document_vectors[indices].mean(axis=0))
+                self.user2id[user] = i
+            self.user_vectors = np.array(user_vectors)
 
     def __dict__(self, user):
         return self.__user_vector(user)
 
-    def encode(self, words):
-        y = self.cv.transform([words]).toarray()[0]
-        d = np.dot(self.R, y)
-        return d
+    def encode(self, X, batch=1000):
+        stacks = []
+        for i in tqdm(range(0, len(X), batch)):
+            j = i + batch
+            bow = self.cv.transform(X[i:j]).toarray()
+            stacks.append(np.dot(bow, self.R.T))
+        if len(X) > len(stacks):
+            bow = self.cv.transform(X[j:]).toarray()
+            stacks.append(np.dot(bow, self.R.T))
+        return np.vstack(stacks)
 
     def similars(self, user, topn=None):
         if not topn:
@@ -54,7 +64,7 @@ class Researcher2Vec():
     def search_by_words(self, words, topn=None):
         if not topn:
             topn = len(self.users)
-        d = self.encode(words)
+        d = self.encode([words])
         all_users = [(u[1], self.cosine_similarity(d, self.user_vectors[u[0]])) for u in enumerate(self.users)] 
         return sorted(all_users, key=lambda u: u[1], reverse=True)[:topn] 
 
