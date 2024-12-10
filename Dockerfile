aab3b50b1e0debe269d546f26e897665e3f70ef4
hiropppe <sergei.0910@gmail.com>
2024-12-10 09:03:18 +0900

    changed to build using poetry.

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM ubuntu:22.04
+
+ENV DEBIAN_FRONTEND noninteractive
+
+WORKDIR /root
+
+RUN apt-get update \
+ && apt-get install -y --no-install-recommends build-essential pkg-config locales tzdata \
+    python3 python3-dev python3-pip python3-wheel python3-venv pipx \
+    vim git curl jq \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN pipx ensurepath
+
+RUN locale-gen ja_JP.UTF-8
+
+ENV LANG ja_JP.UTF-8
+ENV TZ Asia/Tokyo
+ENV PATH="${PATH}:/root/.local/bin"
+
+RUN ln -sf /usr/bin/python3.10 /usr/bin/python3
+RUN ln -s /usr/bin/python3 /usr/bin/python
+
+RUN pipx install poetry
+
+COPY . /text-model
+
+WORKDIR /text-model
+RUN poetry install
+
+CMD ["poetry", "run", "jupyter", "lab", "--allow-root", "--ip=0.0.0.0", "--NotebookApp.token=''", "--no-browser"]
---
+++ build.py
@@ -0,0 +1,46 @@
+import numpy
+import os
+import shutil
+from distutils.command.build_ext import build_ext
+from distutils.core import Distribution, Extension
+
+from Cython.Build import cythonize
+
+extra_compile_args=["-std=c++11", "-O3", "-mtune=native", "-march=native", "-mfpmath=both", "-msse", "-msse2", "-msse3", "-msse4", "-msse4.1", "-msse4.2", "-mfma"]
+include_dirs = [numpy.get_include(), 'cytm/include']
+
+
+def build():
+    extensions = [
+        Extension(
+            "*",
+            ["cytm/*.pyx"],
+            extra_compile_args=extra_compile_args,
+            include_dirs=include_dirs,
+            language="c++",
+        )
+    ]
+    ext_modules = cythonize(
+        extensions,
+        annotate=True,
+        compiler_directives={"binding": True, "language_level": 3},
+    )
+
+    distribution = Distribution({"name": "extended", "ext_modules": ext_modules})
+    distribution.package_dir = "extended"
+
+    cmd = build_ext(distribution)
+    cmd.ensure_finalized()
+    cmd.run()
+
+    # Copy built extensions back to the project
+    for output in cmd.get_outputs():
+        relative_extension = os.path.relpath(output, cmd.build_lib)
+        shutil.copyfile(output, relative_extension)
+        mode = os.stat(relative_extension).st_mode
+        mode |= (mode & 0o444) >> 2
+        os.chmod(relative_extension, mode)
+
+
+if __name__ == "__main__":
+    build()
